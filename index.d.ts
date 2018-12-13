4a147b75788b0ec447af8aeafb6680bbcb102aaf
Sean Lynch <techniq35@gmail.com>
2018-12-13 11:31:33 -0500

    Add "any" default to "TData" for both FetchResult and FetchProps.  Prettier had it's way with the file.

--- index.d.ts
+++ index.d.ts
@@ -1,45 +1,49 @@
-import * as React from 'react'
+import * as React from 'react';
 
 export interface FetchRequestProps {
-  url: string
-  options?: RequestInit
+  url: string;
+  options?: RequestInit;
 }
 
 export interface FetchUpdateOptions {
   ignorePreviousData: boolean;
 }
 
-export interface FetchResult<TData> {
-  data?: TData
-  loading: boolean | null
-  error?: Error
-  request: FetchRequestProps
-  response: Response
-  fetch(url?: string | null, options?: RequestInit, updateOptions?: Partial<FetchUpdateOptions>): Promise<any>;
+export interface FetchResult<TData = any> {
+  data?: TData;
+  loading: boolean | null;
+  error?: Error;
+  request: FetchRequestProps;
+  response: Response;
+  fetch(
+    url?: string | null,
+    options?: RequestInit,
+    updateOptions?: Partial<FetchUpdateOptions>
+  ): Promise<any>;
   clearData(): void;
 }
 
-export type BodyMethods = 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text'
+export type BodyMethods = 'arrayBuffer' | 'blob' | 'formData' | 'json' | 'text';
 
-export interface FetchProps<TData> {
-  url: string
-  options?: RequestInit | (() => RequestInit)
-  manual?: boolean
-  cache?: boolean | object
+export interface FetchProps<TData = any> {
+  url: string;
+  options?: RequestInit | (() => RequestInit);
+  manual?: boolean;
+  cache?: boolean | object;
   as?:
     | 'auto'
     | BodyMethods
     | ((response: TData) => void)
-    | { [type: string]: (res: TData) => Promise<any> }
-  fetchFunction?: (url: string, options: RequestInit) => Promise<any>
-  onDataChange?: (newData: TData, data: TData) => any
-  onChange?: (result: FetchResult<TData>) => void
-  children: (result: FetchResult<TData>) => React.ReactNode | React.ReactNode
+    | { [type: string]: (res: TData) => Promise<any> };
+  fetchFunction?: (url: string, options: RequestInit) => Promise<any>;
+  onDataChange?: (newData: TData, data: TData) => any;
+  onChange?: (result: FetchResult<TData>) => void;
+  children: (result: FetchResult<TData>) => React.ReactNode | React.ReactNode;
 }
 
 export default class Fetch<TData = any> extends React.Component<
   FetchProps<TData>
 > {
   // Passing any to FetchResult as unable to use TData
-  static Consumer: React.Consumer<FetchResult<any>>
+  static Consumer: React.Consumer<FetchResult<any>>;
 }
