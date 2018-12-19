408f8ef9b1120b6c9dc9fb783093fbe4b179910a
Sean Lynch <techniq35@gmail.com>
2018-12-19 16:09:08 -0500

    Fully expose FetchContext as named export (add to index.js)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default, useFetch } from './Fetch';
+export { default, useFetch, FetchContext } from './Fetch';
 export { default as SimpleCache } from './SimpleCache';
 export { parseBody } from './utils';
